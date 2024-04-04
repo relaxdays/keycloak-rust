776baf36fa77ac076a5927b47c109e915afc9aba
Sophie Tauchert <sophie@999eagle.moe>
2024-04-04 09:54:32 +0200

    rust: move into subdirectory

--- nix/crate/default.nix
+++ nix/crate/default.nix
@@ -14,20 +14,16 @@ rustPlatform.buildRustPackage {
     builder = builtins.toFile "builder.sh" ''
       source $stdenv/setup
       mkdir $out
-      cp -rT --no-preserve=mode,ownership $apiExample $out/api-example/
-      cp -rT --no-preserve=mode,ownership $src $out/src/
+      cp -rT --no-preserve=mode,ownership $src $out/rust/
       cp --no-preserve=mode,ownership $cargoToml $out/Cargo.toml
       cp --no-preserve=mode,ownership $cargoLock $out/Cargo.lock
-      cp --no-preserve=mode,ownership $buildRs $out/build.rs
     '';
-    apiExample = ../../api-example;
-    src = ../../src;
+    src = ../../rust;
     cargoToml = ../../Cargo.toml;
     cargoLock = ../../Cargo.lock;
-    buildRs = ../../build.rs;
   };
 
-  buildAndTestSubdir = "api-example";
+  buildAndTestSubdir = "rust/api-example";
   cargoTestFlags = ["--workspace"];
 
   cargoLock.lockFile = ../../Cargo.lock;
