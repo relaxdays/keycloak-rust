3c5979ba982fa7069166aefb7cf7c00fa926accb
Sophie Tauchert <sophie@999eagle.moe>
2024-06-17 15:28:41 +0200

    rust/api: extend role queries to allow for client roles

--- rust/src/api/role.rs
+++ rust/src/api/role.rs
@@ -20,14 +20,30 @@ pub trait KeycloakRoleExt {
     fn role_by_id(&self, role_id: &str) -> impl Future<Output = Result<RoleRepresentation>> + Send;
 
     /// get direct member groups of a role
+    ///
+    /// if a `client_id` is specified, this queries a client role
     fn groups_in_role(
         &self,
+        client_id: Option<&str>,
         role_name: &str,
     ) -> impl Future<Output = Result<Vec<GroupRepresentation>>> + Send;
 
-    /// get all client scopes configured in the realm
+    /// get all members of a role
+    ///
+    /// if `include_indirect` is `true`, members of groups with this role are returned as well
+    fn users_in_role_by_id(
+        &self,
+        role_id: &str,
+        include_indirect: bool,
+    ) -> impl Future<Output = Result<Vec<UserRepresentation>>> + Send;
+
+    /// get all members of a role
+    ///
+    /// if `include_indirect` is `true`, members of groups with this role are returned as well
+    /// if a `client_id` is specified, this queries a client role
     fn users_in_role(
         &self,
+        client_id: Option<&str>,
         role_name: &str,
         include_indirect: bool,
     ) -> impl Future<Output = Result<Vec<UserRepresentation>>> + Send;
@@ -63,22 +79,73 @@ impl<A: crate::AuthenticationProvider + Send + Sync> KeycloakRoleExt for crate::
     }
 
     #[tracing::instrument(skip(self))]
-    async fn groups_in_role(&self, role_name: &str) -> Result<Vec<GroupRepresentation>> {
+    async fn groups_in_role(
+        &self,
+        client_id: Option<&str>,
+        role_name: &str,
+    ) -> Result<Vec<GroupRepresentation>> {
         self.refresh_if_necessary().await?;
         let api_client = self.api_client.read().await;
 
         tracing::debug!("querying groups with role");
-        let groups = api_client
-            .get_realm_role_by_name_groups(&self.config.realm, role_name, Some(true), None, None)
-            .await
-            .map_err(crate::error::progenitor)?
-            .into_inner();
+        let groups = if let Some(client_id) = client_id {
+            paginate_api!(|first, max| {
+                api_client
+                    .get_realm_client_role_by_name_groups(
+                        &self.config.realm,
+                        client_id,
+                        role_name,
+                        Some(true),
+                        Some(first),
+                        Some(max),
+                    )
+                    .await
+                    .map_err(crate::error::progenitor)?
+                    .into_inner()
+            })
+        } else {
+            paginate_api!(|first, max| {
+                api_client
+                    .get_realm_role_by_name_groups(
+                        &self.config.realm,
+                        role_name,
+                        Some(true),
+                        Some(first),
+                        Some(max),
+                    )
+                    .await
+                    .map_err(crate::error::progenitor)?
+                    .into_inner()
+            })
+        };
         Ok(groups)
+    }
+
+    #[tracing::instrument(skip(self))]
+    async fn users_in_role_by_id(
+        &self,
+        role_id: &str,
+        include_indirect: bool,
+    ) -> Result<Vec<UserRepresentation>> {
+        let role = self.role_by_id(role_id).await?;
+
+        if role.client_role == Some(true) {
+            self.users_in_role(
+                Some(&role.container_id.unwrap()),
+                &role.name.unwrap(),
+                include_indirect,
+            )
+            .await
+        } else {
+            self.users_in_role(None, &role.name.unwrap(), include_indirect)
+                .await
+        }
     }
 
     #[tracing::instrument(skip(self))]
     async fn users_in_role(
         &self,
+        client_id: Option<&str>,
         role_name: &str,
         include_indirect: bool,
     ) -> Result<Vec<UserRepresentation>> {
@@ -86,14 +153,37 @@ impl<A: crate::AuthenticationProvider + Send + Sync> KeycloakRoleExt for crate::
         let api_client = self.api_client.read().await;
 
         tracing::debug!("querying users with role");
-        let mut users = api_client
-            .get_realm_role_by_name_users(&self.config.realm, role_name, None, None)
-            .await
-            .map_err(crate::error::progenitor)?
-            .into_inner();
+        let mut users = if let Some(client_id) = client_id {
+            paginate_api!(|first, max| {
+                api_client
+                    .get_realm_client_role_by_name_users(
+                        &self.config.realm,
+                        client_id,
+                        role_name,
+                        Some(first),
+                        Some(max),
+                    )
+                    .await
+                    .map_err(crate::error::progenitor)?
+                    .into_inner()
+            })
+        } else {
+            paginate_api!(|first, max| {
+                api_client
+                    .get_realm_role_by_name_users(
+                        &self.config.realm,
+                        role_name,
+                        Some(first),
+                        Some(max),
+                    )
+                    .await
+                    .map_err(crate::error::progenitor)?
+                    .into_inner()
+            })
+        };
 
         if include_indirect {
-            let groups = self.groups_in_role(role_name).await?;
+            let groups = self.groups_in_role(client_id, role_name).await?;
             for group in &groups {
                 let mut group_users = self
                     .group_users(group.id.as_ref().unwrap(), Some(true))
